d03371c7f16b38a5822e0d2b65455853c684d6e6
sjcaci <shawn.johnson@caci.com>
2021-11-17 16:35:57 -0700

    removed hello

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-  ],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
